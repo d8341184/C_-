fa96fd2ae3a3e35c1f16133a061b0c161f86f1c5
d8341184 <d8341184@yahoo.com.tw>
2020-09-14 12:00:11 +0800

    Add files via upload

---
+++ ch02_03.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	short int number1=0300;
+	int number2=0x33f;
+	long int number3=-123456789;
+	unsigned long int number4=978654321;
+	cout<<"短整數"<<number1<<"所佔位元組"<<sizeof(number1)<<endl;
+	cout<<"整數"<<number2<<"所佔位元組"<<sizeof(number2)<<endl;
+	cout<<"長整數"<<number3<<"所佔位元組"<<sizeof(number3)<<endl;
+	cout<<"無號長整數"<<number4<<"所佔位元組"<<sizeof(number4)<<endl;
+	return 0;
+}
+//短整數192所佔位元組2
+//整數831所佔位元組4
+//長整數-123456789所佔位元組4
+//無號長整數978654321所佔位元組4
