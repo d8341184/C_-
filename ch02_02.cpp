ca0c99d1d3612d11a747c53b8d04bc2fc3a8c37a
d8341184 <d8341184@yahoo.com.tw>
2020-09-14 12:01:28 +0800

    Add files via upload

---
+++ ch02_02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#define PI 3.14159
+
+int main(){
+	const int radius = 10;
+	cout<<"¶êªº¥b®|="<<radius<<"¡A­±¿n"<<radius*radius*PI<<endl;
+	
+	return 0; 
+}
